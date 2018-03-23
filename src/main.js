f95b248fa605cdd8420b9ea0908d03a493e0c3ae
Maicol Santos <maicol.santos@gsd.com.br>
2018-03-22 21:12:57 -0300

    Instalação
    Instalado a base do projeto Vue.JS, Vuex, Vue Router, Vuetify

---
+++ src/main.js
@@ -0,0 +1,34 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import Vuex from 'vuex'
+import 'vuetify/dist/vuetify.min.css'
+import Vuetify from 'vuetify'
+import * as moment from 'moment'
+import colors from 'vuetify/es5/util/colors'
+
+import App from './App'
+import router from './router'
+import store from './store'
+
+Vue.config.productionTip = false
+
+const optionsColors = {
+  theme: {
+    primary: colors.yellow.lighten1,
+    secondary: colors.yellow.darken3,
+    accent: colors.indigo.base
+  }
+}
+
+Vue.use(Vuetify, optionsColors, Vuex)
+moment.locale(process.env.locale)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  components: { App },
+  template: '<App/>'
+})
